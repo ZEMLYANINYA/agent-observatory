f9b6cd2445b82cc829ecfd18b76dd0e63209cb2c
Aleksandr <p4elovodof@gmail.com>
2026-09-16 21:59:25 +0200

    feat(evidence): add endpoint event adapter package

---
+++ src/agent_observatory/evidence/__init__.py
@@ -0,0 +1,23 @@
+from .endpoint_events import (
+    ApplicationDiscoveryOutcome,
+    application_discovery_event,
+    executable_evidence_events,
+    file_hash_event,
+    file_identity_event,
+    operator_marker_event,
+    process_observed_event,
+    process_relationship_event,
+    tcp_connection_event,
+)
+
+__all__ = [
+    "ApplicationDiscoveryOutcome",
+    "application_discovery_event",
+    "executable_evidence_events",
+    "file_hash_event",
+    "file_identity_event",
+    "operator_marker_event",
+    "process_observed_event",
+    "process_relationship_event",
+    "tcp_connection_event",
+]
